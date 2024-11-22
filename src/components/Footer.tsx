9f47dadbabdf7363a1660060347c2f7cf9d3d2aa
Tomás <tomasplleonart@gmail.com>
2024-11-21 22:28:56 -0300

    cambio url instagram e idiomismo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,13 @@ const Footer: FC = () => {
     return (
         <footer className="mb-12 text-sm text-gray-500 flex flex-col justify-center items-center gap-2">
             <a
-                href="https://www.instagram.com/corrientealterna"
+                href="https://www.instagram.com/corrientealternaok"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex items-center text-crayola hover:text-orange-500 transition-colors"
             >
                 <Instagram className="mr-2" />
-                Síguenos en Instagram
+                Seguinos en Instagram
             </a>
             © 2023 Corriente Alterna. Todos los derechos reservados.
         </footer>

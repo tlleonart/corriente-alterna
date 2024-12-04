02ffc20eb766a8b20478942dff81b6758f348cc7
Tomás <tomasplleonart@gmail.com>
2024-12-04 16:12:09 -0300

    cambio de color del texto de modal

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
                 <div className="flex flex-col justify-center h-screen w-screen relative z-50 items-center ">
                     <div className="gap-4 bg-black opacity-100 h-2/3 w-2/3 border-white-[1px] flex flex-col justify-center text-center p-4">
                         <h1 className="text-xl text-crayola"> INFORMACIÓN IMPORTANTE</h1>
-                        <p>La locación del evento se trasladó al Museo de la Ciudad (Bv. Oroño 2361). Si ya te acreditaste, no es necesario que vuelvas a llenar el formulario. Las acreditaciones llegarán el 10 de diciembre al mail que ingresaste. Si no recibís tu acreditación, contactate a somoscorrientealterna@gmail.com</p>
+                        <p className='text-white'>La locación del evento se trasladó al Museo de la Ciudad (Bv. Oroño 2361). Si ya te acreditaste, no es necesario que vuelvas a llenar el formulario. Las acreditaciones llegarán el 10 de diciembre al mail que ingresaste. Si no recibís tu acreditación, contactate a somoscorrientealterna@gmail.com</p>
                         <Button onClick={() => setModalClosed(true)}>ENTENDIDO</Button>
                     </div>
                 </div>
